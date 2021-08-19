b0d6dc408b82c27b6c358800a7679065ce5f7cbe
youngsoooh <oh.youngsoo23@gmail.com>
2021-08-19 14:08:22 +0900

    test

---
+++ src/main/java/com/jpractice/JpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpractice/JpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
